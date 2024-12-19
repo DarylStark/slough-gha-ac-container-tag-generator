55f48c9e2fd55df2ff4fd77eb86dca15c697d26c
Daryl Stark <daryl.stark@asml.com>
2024-12-19 08:39:44 +0100

    chore: Basic file structure and metadata

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slough - GitHub Action - Determine tags for Docker container
+
+GitHub action to generate a list of tags for a containerimage, based on the branch and the commit hash.
---
+++ src/generate-tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version=$1
+echo $version
+
+export REPO_NAME=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 2)
+export MAIN_TAG="${GITHUB_REF_NAME}"
+if [ "${{ github.ref_name }}" == "main" ]; then
+    export MAIN_TAG="latest"
+elif [ "${{ github.ref_name }}" == "dev" ]; then
+    export MAIN_TAG="latest-dev"
+fi
+echo "tag-name="${DOCKER_USERNAME}/${REPO_NAME}:${MAIN_TAG},${DOCKER_USERNAME}/${REPO_NAME}:${GITHUB_SHA}"" # >> "$GITHUB_OUTPUT"
